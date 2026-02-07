b6d3dfbce4c09046e5d1db02086423d5c8c81275
Matthias Kerat <MatthiasKerat@tapdo.io>
2026-02-07 20:58:16 +0100

    fix: change paths again

--- public/api/send_contact.php
+++ public/api/send_contact.php
@@ -8,9 +8,12 @@ require __DIR__ . '/PHPMailer/src/Exception.php';
 require __DIR__ . '/PHPMailer/src/PHPMailer.php';
 require __DIR__ . '/PHPMailer/src/SMTP.php';
 
-$secrets = require $_SERVER['DOCUMENT_ROOT'] . '/../config/secrets.php';
+$secrets = require __DIR__ . '/../../config/secrets.php';
 
 header('Content-Type: application/json; charset=utf-8');
+
+error_log('__DIR__=' . __DIR__);
+error_log('DOCROOT=' . ($_SERVER['DOCUMENT_ROOT'] ?? ''));
 
 require __DIR__ . '/_rate_limit.php';
 
