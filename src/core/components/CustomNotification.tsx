e1e52069163f7ec82c2a660cfcefb699bfe51f7d
Matthias Kerat <MatthiasKerat@tapdo.io>
2026-02-07 17:31:34 +0100

    fix: give notification higher z index

--- src/core/components/CustomNotification.tsx
+++ src/core/components/CustomNotification.tsx
@@ -51,7 +51,7 @@ export function CustomNotification({
   const notification = (
     <div
       aria-live="assertive"
-      className="pointer-events-none z-50 fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+      className="pointer-events-none z-[9999] fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
     >
       <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
         <Transition show={show}>
